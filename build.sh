d89a71151dcd5b838ff03ef9bbfa2456a4bf2c16
Brian Jones <brian.j.jones@intel.com>
2022-05-05 17:51:26 -0700

    Upgraded perf numbers for OpenVINO
    This gives greater detail and control when trying
    to benchmark OpenVINO.

--- build.sh
+++ build.sh
@@ -9,32 +9,93 @@ else
         echo "Please specify as or rust to build"
     else
         BUILD_TYPE=$1
+        BACKEND=$2
+        PERF=$4
+        LOOP_SIZE=$5
+        export BACKEND=$BACKEND
+
+        if [ -z "$3" ]; then MODEL="mobilenet_v2"; else MODEL=$3; fi
+        if [ -z "$5" ]; then LOOP_SIZE=1; else LOOP_SIZE=$5; fi
+        export MODEL=$MODEL
+        export LOOP_SIZE=$LOOP_SIZE
         WASI_NN_DIR=$(dirname "$0" | xargs dirname)
         WASI_NN_DIR=$(realpath $WASI_NN_DIR)
-        source /opt/intel/openvino/bin/setupvars.sh
 
         case $BUILD_TYPE in
             as)
-            pushd $WASI_NN_DIR/assemblyscript
+
+                pushd $WASI_NN_DIR/assemblyscript
                 npm install
-                npm run demo
-            ;;
+
+                case $BACKEND in
+                    openvino)
+                        npm run openvino
+                        ;;
+                    *)
+                        echo "Unknown backend, please enter 'openvino'"
+                        exit;
+                        ;;
+                esac
+                ;;
 
             rust)
                 echo "The first argument: $1"
-                FIXTURE=https://github.com/intel/openvino-rs/raw/main/crates/openvino/tests/fixtures/mobilenet
                 pushd $WASI_NN_DIR/rust/
                 cargo build --release --target=wasm32-wasi
                 mkdir -p $WASI_NN_DIR/rust/examples/classification-example/build
                 RUST_BUILD_DIR=$(realpath $WASI_NN_DIR/rust/examples/classification-example/build/)
                 cp -rn images $RUST_BUILD_DIR
                 pushd examples/classification-example
-                cargo build --release --target=wasm32-wasi
+                export MAPDIR="fixture"
+
+                case $PERF in
+                    perf)
+                        case $LOOP_SIZE in
+                            ''|*[!0-9]*)
+                                echo "Loop size needs to be a number";
+                                exit;
+                                ;;
+                        esac
+                        echo "RUNNING PERFORMANCE CHECKS"
+                        cargo build --release --target=wasm32-wasi --features performance
+                        ;;
+                    *)
+                        cargo build --release --target=wasm32-wasi
+                        ;;
+                esac
+
                 cp target/wasm32-wasi/release/wasi-nn-example.wasm $RUST_BUILD_DIR
+
+                case $BACKEND in
+                    openvino)
+                        echo "Using OpenVino"
+                        source /opt/intel/openvino/bin/setupvars.sh
+                        RUST_DIR=$(realpath $WASI_NN_DIR/rust/examples/classification-example/)
+                        FIXTURE=https://github.com/intel/openvino-rs/raw/main/crates/openvino/tests/fixtures
+
+                        if [ ! -f "models/mobilenet_v2/model.bin" ]
+                        then
+                            wget -O models/mobilenet_v2/model.bin --no-clobber $FIXTURE/mobilenet/mobilenet.bin
+                            wget -O models/mobilenet_v2/model.xml --no-clobber $FIXTURE/mobilenet/mobilenet.xml
+                        fi
+                        if [ ! -f "models/inception_v3/model.bin" ]
+                        then
+                            wget -O models/inception_v3/model.bin --no-clobber $FIXTURE/inception/inception.bin
+                            wget -O models/inception_v3/model.xml --no-clobber $FIXTURE/inception/inception.xml
+                        fi
+
+                        cp models/$MODEL/model.bin $RUST_BUILD_DIR
+                        cp models/$MODEL/model.xml $RUST_BUILD_DIR
+                        cp models/$MODEL/tensor.desc $RUST_BUILD_DIR
+                        ;;
+                    *)
+                        echo "Unknown backend, please enter 'openvino'"
+                        exit;
+                        ;;
+                esac
+
                 pushd build
-                wget --no-clobber --directory-prefix=$RUST_BUILD_DIR $FIXTURE/mobilenet.bin
-                wget --no-clobber --directory-prefix=$RUST_BUILD_DIR $FIXTURE/mobilenet.xml
-                wasmtime run --mapdir fixture::$RUST_BUILD_DIR wasi-nn-example.wasm --wasi-modules=experimental-wasi-nn
+                wasmtime run --mapdir fixture::$RUST_BUILD_DIR  wasi-nn-example.wasm --wasi-modules=experimental-wasi-nn
             ;;
             *)
                 echo "Unknown build type $BUILD_TYPE"
--- rust/examples/classification-example/src/main.rs
+++ rust/examples/classification-example/src/main.rs
@@ -1,83 +1,155 @@
 use image::{DynamicImage};
 use image::io::Reader;
-use std::convert::TryInto;
+use std::{convert::TryInto, str::FromStr};
 use std::fs;
 use wasi_nn;
 mod imagenet_classes;
+use std::env;
+use std::time::{Duration, Instant};
 
 pub fn main() {
-    let xml = fs::read_to_string("fixture/mobilenet.xml").unwrap();
-    println!("Read graph XML, first 50 characters: {}", &xml[..50]);
+    let loop_size: u32 = env!("LOOP_SIZE").parse().unwrap();
+    let tensor_desc_data = fs::read_to_string("fixture/tensor.desc").unwrap();
+    let tensor_desc = TensorDescription::from_str(&tensor_desc_data).unwrap();
+    match env!("BACKEND") {
+        "openvino" => {
+            println!("##################################################################\n");
+            println!("Running benchmark using OpenVINO, {} model, and looping for {} times...\n", env!("MODEL"), loop_size);
+            println!("##################################################################");
+            execute(wasi_nn::GRAPH_ENCODING_OPENVINO, &tensor_desc.dimensions(), vec![0f32; 1001], loop_size);
+        },
+        _ => {
+            println!("Unknown backend, exiting...");
+            return();
+        }
+    }
+}
 
-    let weights = fs::read("fixture/mobilenet.bin").unwrap();
-    println!("Read graph weights, size in bytes: {}", weights.len());
+fn execute(backend: wasi_nn::GraphEncoding, dimensions: &[u32], mut output_buffer: Vec<f32>, loop_size: u32) {
+    println!("** Using the {} backend **", backend);
+
+    // #[cfg(feature = "performance")]
+    let init_time = Instant::now();
+    // #[cfg(feature = "performance")]
+    let mut id_secs: Duration;
+
+    let gba: Vec<Vec<u8>> = create_gba(backend);
 
     let graph = unsafe {
         wasi_nn::load(
-            &[&xml.into_bytes(), &weights],
-            wasi_nn::GRAPH_ENCODING_OPENVINO,
+            &[&gba[0], &gba[1]],
+            backend,
             wasi_nn::EXECUTION_TARGET_CPU,
         )
         .unwrap()
     };
-    println!("Loaded graph into wasi-nn with ID: {}", graph);
 
+    println!("Loaded graph into wasi-nn with ID: {}", graph);
     let context = unsafe { wasi_nn::init_execution_context(graph).unwrap() };
+
+    // #[cfg(feature = "performance")]
+    let init_secs = init_time.elapsed();
+
     println!("Created wasi-nn execution context with ID: {}", context);
+    #[cfg(feature = "performance")]
+    printtime("Initiating the backend", init_secs.as_micros());
 
     // Load a tensor that precisely matches the graph input tensor (see
-    // `fixture/frozen_inference_graph.xml`).
     for i in 0..5 {
         let filename: String = format!("{}{}{}", "fixture/images/", i, ".jpg");
-        let tensor_data = image_to_tensor(filename, 224, 224);
-        println!("Read input tensor, size in bytes: {}", tensor_data.len());
+
+        let tensor_data: Vec<u8> = image_to_tensor(filename, dimensions[2], dimensions[3], backend);
+
+
         let tensor = wasi_nn::Tensor {
-            dimensions: &[1, 3, 224, 224],
-            r#type: wasi_nn::TENSOR_TYPE_F32,
-            data: &tensor_data,
-        };
+                        dimensions: dimensions,
+                        r#type: wasi_nn::TENSOR_TYPE_F32,
+                        data: &tensor_data,
+                    };
+
         unsafe {
             wasi_nn::set_input(context, 0, tensor).unwrap();
         }
 
-        // Execute the inference.
-        unsafe {
-            wasi_nn::compute(context).unwrap();
-        }
-        println!("Executed graph inference");
+        // #[cfg(feature = "performance")]
+        let mut totaltime = 0;
+        for j in 0..loop_size {
+            // #[cfg(feature = "performance")]
+            let id_time = Instant::now();
+            // Execute the inference.
+            unsafe {
+                wasi_nn::compute(context).unwrap();
+            }
+            id_secs = id_time.elapsed();
+            totaltime += id_secs.as_micros();
 
-        // Retrieve the output.
-        let mut output_buffer = vec![0f32; 1001];
-        unsafe {
-            wasi_nn::get_output(
-                context,
-                0,
-                &mut output_buffer[..] as *mut [f32] as *mut u8,
-                (output_buffer.len() * 4).try_into().unwrap(),
-            )
-            .unwrap();
-        }
+            #[cfg(feature = "performance")]
+            if j == 1 {
+                printtime("First run took", totaltime);
+            }
 
-        let results = sort_results(&output_buffer);
-        println!(
-            "Found results, sorted top 5: {:?}",
-            &results[..5]
-        );
+            if j == loop_size - 1 {
+                #[cfg(feature = "performance")]
+                let msg = format!("{}{}", loop_size, " runs took");
+                #[cfg(feature = "performance")]
+                printtime(&msg,  totaltime);
 
-        for i in 0..5 {
-            println!("{}.) {}", i + 1, imagenet_classes::IMAGENET_CLASSES[results[i].0]);
+                unsafe {
+                    wasi_nn::get_output(
+                        context,
+                        0,
+                        &mut output_buffer[..] as *mut [f32] as *mut u8,
+                        (output_buffer.len() * 4).try_into().unwrap(),
+                    )
+                    .unwrap();
+                }
+
+                let results = sort_results(&output_buffer, backend);
+                println!(
+                    "Found results, sorted top 5: {:?}",
+                    &results[..5]
+                );
+
+
+                for i in 0..5 {
+                    println!("{}.) {}", i + 1, imagenet_classes::IMAGENET_CLASSES[results[i].0]);
+                }
+            }
         }
     }
+}
 
+fn create_gba (backend: u8) -> Vec<Vec<u8>> {
+    let result: Vec<Vec<u8>> = match backend {
+        wasi_nn::GRAPH_ENCODING_OPENVINO => {
+            let xml = fs::read_to_string("fixture/model.xml").unwrap();
+            println!("Read graph XML, first 50 characters: {}", &xml[..50]);
+            let weights = fs::read("fixture/model.bin").unwrap();
+            println!("Read graph weights, size in bytes: {}", weights.len());
+
+            Vec::from([xml.into_bytes(), weights])
+        },
+        _ => {
+            println!("Unknown backend {}", backend);
+            vec![]
+        }
+
+    };
+    return result;
 }
 
 // Sort the buffer of probabilities. The graph places the match probability for each class at the
 // index for that class (e.g. the probability of class 42 is placed at buffer[42]). Here we convert
 // to a wrapping InferenceResult and sort the results.
-fn sort_results(buffer: &[f32]) -> Vec<InferenceResult> {
+fn sort_results(buffer: &[f32], backend: u8) -> Vec<InferenceResult> {
+    let skipval = match backend {
+        wasi_nn::GRAPH_ENCODING_OPENVINO => { 1 },
+        _ => { 0 }
+    };
+
     let mut results: Vec<InferenceResult> = buffer
         .iter()
-        .skip(1)
+        .skip(skipval)
         .enumerate()
         .map(|(c, p)| InferenceResult(c, *p))
         .collect();
@@ -85,29 +157,99 @@ fn sort_results(buffer: &[f32]) -> Vec<InferenceResult> {
     results
 }
 
-// Take the image located at 'path', open it, resize it to height x width, and then converts
-// the pixel precision to FP32. The resulting BGR pixel vector is then returned.
-fn image_to_tensor(path: String, height: u32, width: u32) -> Vec<u8> {
-    let pixels = Reader::open(path).unwrap().decode().unwrap();
-    let dyn_img: DynamicImage = pixels.resize_exact(width, height, image::imageops::Triangle);
-    let bgr_img = dyn_img.to_bgr8();
-    // Get an array of the pixel values
-    let raw_u8_arr: &[u8] = &bgr_img.as_raw()[..];
-    // Create an array to hold the f32 value of those pixels
-    let bytes_required = raw_u8_arr.len() * 4;
-    let mut u8_f32_arr:Vec<u8> = vec![0; bytes_required];
 
-    for i in 0..raw_u8_arr.len()  {
-        // Read the number as a f32 and break it into u8 bytes
-        let u8_f32: f32 = raw_u8_arr[i] as f32;
-        let u8_bytes = u8_f32.to_ne_bytes();
 
-        for j in 0..4 {
-            u8_f32_arr[(i * 4) + j] = u8_bytes[j];
+fn image_to_tensor(path: String, height: u32, width: u32, backend: u8) -> Vec<u8> {
+    let result: Vec<u8> = match backend {
+        wasi_nn::GRAPH_ENCODING_OPENVINO => {
+            let pixels = Reader::open(path).unwrap().decode().unwrap();
+            let dyn_img: DynamicImage = pixels.resize_exact(width, height, image::imageops::Triangle);
+            let bgr_img = dyn_img.to_bgr8();
+            // Get an array of the pixel values
+            let raw_u8_arr: &[u8] = &bgr_img.as_raw()[..];
+            // Create an array to hold the f32 value of those pixels
+            let bytes_required = raw_u8_arr.len() * 4;
+            let mut u8_f32_arr:Vec<u8> = vec![0; bytes_required];
+
+            for i in 0..raw_u8_arr.len()  {
+                // Read the number as a f32 and break it into u8 bytes
+                let u8_f32: f32 = raw_u8_arr[i] as f32;
+                let u8_bytes = u8_f32.to_ne_bytes();
+
+                for j in 0..4 {
+                    u8_f32_arr[(i * 4) + j] = u8_bytes[j];
+                }
+            }
+
+            u8_f32_arr
+        },
+        _ => {
+            println!("Unknown backend {}", backend);
+            vec![]
         }
-    }
-    return u8_f32_arr;
+    };
+    return result;
 }
+
 // A wrapper for class ID and match probabilities.
 #[derive(Debug, PartialEq)]
 struct InferenceResult(usize, f32);
+
+#[cfg(feature = "performance")]
+fn printtime (msg: &str, dur: u128) {
+    let bdrlen = (msg.len() + 20) as u16;
+
+    for _i in 0..bdrlen {
+        print!("-");
+    }
+
+    let durstr = dur.to_string();
+    let charnum = durstr.chars().count();
+
+    println!("");
+    println!("** {} took {}.{} ms **", msg, &durstr[0..charnum-3], &durstr[(charnum-3)..charnum]);
+
+    for _i in 0..bdrlen {
+        print!("-");
+    }
+    println!("");
+}
+
+/// This structure makes it possible to use runtime-defined tensor dimensions by reading the
+/// tensor description from a string, e.g. `TensorDescription::from_str("u8x1x3x300x300")`.
+struct TensorDescription(wasi_nn::TensorType, Vec<u32>);
+
+impl TensorDescription {
+    pub fn precision(&self) -> wasi_nn::TensorType {
+        self.0
+    }
+    pub fn dimensions<'a>(&'a self) -> &'a [u32] {
+        &self.1
+    }
+}
+
+impl FromStr for TensorDescription {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.trim().split('x').collect();
+        if parts.len() < 2 {
+            return Err(
+                "Not enough parts in description string; should be [precision]x[dimension]*",
+            );
+        }
+        let precision = match parts[0] {
+            "u8" => wasi_nn::TENSOR_TYPE_U8,
+            "i32" => wasi_nn::TENSOR_TYPE_I32,
+            "f16" => wasi_nn::TENSOR_TYPE_F16,
+            "f32" => wasi_nn::TENSOR_TYPE_F32,
+            _ => return Err("Unknown precision string; should be one of [u8|i32|f16|f32]"),
+        };
+        let mut dimensions = Vec::new();
+        for part in parts.into_iter().skip(1) {
+            let dimension = u32::from_str(part).map_err(|_| "Failed to parse dimension as i32")?;
+            dimensions.push(dimension);
+        }
+        Ok(Self(precision, dimensions))
+    }
+}
